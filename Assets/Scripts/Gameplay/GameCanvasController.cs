076231aa2560e8bc096d83220def5048b8546ea3
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2019-12-18 00:56:59 +0100

    Add a couple of tooltips

--- Assets/Scripts/Gameplay/GameCanvasController.cs
+++ Assets/Scripts/Gameplay/GameCanvasController.cs
@@ -10,8 +10,10 @@ namespace MadsBangH.ArcheryGame
 		private static readonly int ScoreBumpedHash = Animator.StringToHash("Score Bumped");
 
 		[SerializeField]
+		[Tooltip("Label to update text of when score is increased")]
 		private TMP_Text scoreLabel = default;
 		[SerializeField]
+		[Tooltip("Animator to trigger \"Score Bumped\" on when score is increased")]
 		private Animator scoreLabelAnimator = default;
 
 		private void OnEnable()
